1da9cd76ea55db7f6075d4b15ff35ccd14369b4e
Sean Kim <sean@latch.com>
2019-01-12 22:44:05 -0500

    added express

---
+++ src/Update.elm
@@ -0,0 +1,23 @@
+module Update exposing (update)
+
+import Browser
+import Browser.Navigation as Nav
+import Model exposing (Model, Msg(..))
+import Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }
+            , Cmd.none
+            )

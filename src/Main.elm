91b6f6a2c04c79c2473e3829db3af6520ed87901
Sean Kim <sean@latch.com>
2019-01-12 22:43:07 -0500

    works with express

---
+++ src/Main.elm
@@ -0,0 +1,38 @@
+module Main exposing (init, main, subscriptions)
+
+import Browser
+import Browser.Navigation as Nav
+import Model exposing (..)
+import Update exposing (..)
+import Url
+import View exposing (..)
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( Model key url, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none

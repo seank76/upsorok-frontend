b37545b4b20369616b1b7abe3332745bfb5999f5
Sean Kim <sean@latch.com>
2019-01-26 21:51:25 -0500

    latest

---
+++ src/Model/Main.elm
@@ -0,0 +1,26 @@
+module Model.Main exposing (ALink, Model, Msg(..))
+
+import Browser
+import Browser.Navigation as Nav
+import Url
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { key : Nav.Key
+    , url : Url.Url
+    }
+
+
+type alias ALink =
+    { link : String
+    , name : String
+    }
+
+
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
